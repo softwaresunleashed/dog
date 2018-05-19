6c13bc6922648e4b3f9fe1ecf4e16d5f08f26d75
Sudhanshu Gupta <sudhanshu.gupta@nxp.com>
2018-05-19 14:18:41 +0530

    NPI Details Database Added

---
+++ app/src/main/java/com/softwaresunleashed/dog/database/NPISelectionDatabaseDetails.java
@@ -0,0 +1,42 @@
+package com.softwaresunleashed.dog.database;
+
+public class NPISelectionDatabaseDetails {
+
+
+    public static String[][] db_details = {
+            {"LA1575-simulator", "LA1575-simulator.sqlite"},
+            {"LA1575", "LA1575.sqlite"},
+            {"LA1785-simulator", "LA1785-simulator.sqlite"},
+            {"LS1012A", "LS1012A.sqlite"},
+            {"LS1023A", "LS1023A.sqlite"},
+            {"LS1026A", "LS1026A.sqlite"},
+            {"LS1028A-emu", "LS1028A-emu.sqlite"},
+            {"LS1028A-simulator", "LS1028A-simulator.sqlite"},
+            {"LS1028A", "LS1028A.sqlite"},
+            {"LS1043A-simulator", "LS1043A-simulator.sqlite"},
+            {"LS1043A", "LS1043A.sqlite"},
+            {"LS1044A", "LS1044A.sqlite"},
+            {"LS1046A-simulator", "LS1046A-simulator.sqlite"},
+            {"LS1046A", "LS1046A.sqlite"},
+            {"LS1048A", "LS1048A.sqlite"},
+            {"LS1084A", "LS1084A.sqlite"},
+            {"LS1088A-simulator", "LS1088A-simulator.sqlite"},
+            {"LS1088A", "LS1088A.sqlite"},
+            {"LS2040A", "LS2040A.sqlite"},
+            {"LS2041A", "LS2041A.sqlite"},
+            {"LS2044A", "LS2044A.sqlite"},
+            {"LS2045A-simulator", "LS2045A-simulator.sqlite"},
+            {"LS2045A", "LS2045A.sqlite"},
+            {"LS2048A", "LS2048A.sqlite"},
+            {"LS2080A", "LS2080A.sqlite"},
+            {"LS2081A", "LS2081A.sqlite"},
+            {"LS2084A", "LS2084A.sqlite"},
+            {"LS2085A-simulator", "LS2085A-simulator.sqlite"},
+            {"LS2085A", "LS2085A.sqlite"},
+            {"LS2088A-simulator", "LS2088A-simulator.sqlite"},
+            {"LS2088A", "LS2088A.sqlite"},
+            {"LX2160A-simulator", "LX2160A-simulator.sqlite"},
+            {"LX2160A", "LX2160A.sqlite"}
+    };
+
+}
