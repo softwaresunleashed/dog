e1c3463a60d5cfdca307fb36f29ad9b92686037b
Sudhanshu Gupta <sudhanshu.gupta@nxp.com>
2018-05-20 20:44:53 +0530

    Code Cleanup + Special Debug Registers updated

--- app/src/main/java/com/softwaresunleashed/dog/debugregs/CPSR_DebugRegisters.java
+++ app/src/main/java/com/softwaresunleashed/dog/debugregs/CPSR_DebugRegisters.java
@@ -1,13 +1,16 @@
 package com.softwaresunleashed.dog.debugregs;
 
-public class CPSR_DebugRegisters implements DebugRegisters {
+public class CPSR_DebugRegisters extends DebugRegisters {
+
+    private String regDesc = "";
 
     public CPSR_DebugRegisters() {
+        setRegister_description(regDesc);
     }
 
     @Override
     public String populate_description_view(String value) {
-        String display = "CPSR : " + value + "\n";
+        String display = "CPSR : " + value + "\n" + getRegister_description();
         return display;
     }
 }
