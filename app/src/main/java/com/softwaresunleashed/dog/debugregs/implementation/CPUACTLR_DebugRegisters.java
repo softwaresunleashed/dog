890ab03965b355fa3de9fde25bee1102fd97e2de
Sudhanshu Gupta <sudhanshu.gupta@nxp.com>
2018-05-27 05:31:10 +0530

    * Added CPUACTLR register description. * Removed one test code (Hardcoded register value)

--- app/src/main/java/com/softwaresunleashed/dog/debugregs/implementation/CPUACTLR_DebugRegisters.java
+++ app/src/main/java/com/softwaresunleashed/dog/debugregs/implementation/CPUACTLR_DebugRegisters.java
@@ -6,187 +6,174 @@ public class CPUACTLR_DebugRegisters extends DebugRegisters {
 
     private String regName = "CPUACTLR";
 
-    private String regDesc = "[63:45]\t-\tReserved, res0.\n" +
-            "[44]\tENDCCASCI\t\n" +
-            "Enable data cache clean as data cache clean/invalidate. The possible values are:\n" +
-            "\n" +
-            "0\n" +
-            "Normal behavior, data cache clean operations are unaffected. This is the reset value.\n" +
-            "\n" +
-            "1\n" +
-            "Executes data cache clean operations as data cache clean and invalidate. The following operations are affected:\n" +
-            "\n" +
-            "In AArch32, DCCSW is executed as DCCISW, DCCMVAU and DCCMVAC are executed as DCCIMVAC.\n" +
-            "\n" +
-            "In AArch64, DC CSW is executed as DC CISW, DC CVAU and DC CVAC are executed as DC CIVAC.\n" +
-            "\n" +
-            "[43:31\t-\tReserved, res0.\n" +
-            "[30]\tFPDIDIS\t\n" +
-            "Disable floating-point dual issue. The possible values are:\n" +
-            "\n" +
-            "0\n" +
-            "Enable dual issue of floating-point, Advanced SIMD and Cryptography instructions. This is the reset value.\n" +
-            "\n" +
-            "1\n" +
-            "Disable dual issue of floating-point, Advanced SIMD and Cryptography instructions.\n" +
-            "\n" +
-            "[29]\tDIDIS\t\n" +
-            "Disable Dual Issue. The possible values are:\n" +
-            "\n" +
-            "0\n" +
-            "Enable Dual Issue of instructions. This is the reset value.\n" +
-            "\n" +
-            "1\n" +
-            "Disable Dual Issue of all instructions.\n" +
-            "\n" +
-            "[28:27]\tRADIS\t\n" +
-            "Write streaming no-allocate threshold. The possible values are:\n" +
-            "\n" +
-            "0b00\n" +
-            "16th consecutive streaming cache line does not allocate in the L1 or L2 cache.\n" +
-            "\n" +
-            "0b01\n" +
-            "128th consecutive streaming cache line does not allocate in the L1 or L2 cache. This is the reset value.\n" +
-            "\n" +
-            "0b10\n" +
-            "512th consecutive streaming cache line does not allocate in the L1 or L2 cache.\n" +
-            "\n" +
-            "0b11\n" +
-            "Disables streaming. All write-allocate lines allocate in the L1 or L2 cache.\n" +
-            "\n" +
-            "[26:25]\tL1RADIS\t\n" +
-            "Write streaming no-L1-allocate threshold. The possible values are:\n" +
-            "\n" +
-            "0b00\n" +
-            "4th consecutive streaming cache line does not allocate in the L1 cache. This is the reset value.\n" +
-            "\n" +
-            "0b01\n" +
-            "64th consecutive streaming cache line does not allocate in the L1 cache.\n" +
-            "\n" +
-            "0b10\n" +
-            "128th consecutive streaming cache line does not allocate in the L1 cache.\n" +
-            "\n" +
-            "0b11\n" +
-            "Disables streaming. All write-allocate lines allocate in the L1 cache.\n" +
-            "\n" +
-            "[24]\tDTAH\t\n" +
-            "Disable Transient allocation hint. The possible values are:\n" +
-            "\n" +
-            "0\n" +
-            "Normal operation.\n" +
-            "\n" +
-            "1\n" +
-            "The Transient allocation hint in the MAIR is ignored and treated the same as non-transient allocation types. The LDNP and STNP instructions in AArch64 behave the same as the equivalent LDP and STP instructions. This is the reset value.\n" +
-            "\n" +
-            "[23]\tSTBPFRS\t\n" +
-            "Disable ReadUnique request for prefetch streams initiated by STB accesses:\n" +
-            "\n" +
-            "0\n" +
-            "ReadUnique used for prefetch streams initiated from STB accesses. This is the reset value.\n" +
-            "\n" +
-            "1\n" +
-            "ReadShared used for prefetch streams initiated from STB accesses.\n" +
-            "\n" +
-            "[22]\tSTBPFDIS\t\n" +
-            "Disable prefetch streams initiated from STB accesses:\n" +
-            "\n" +
-            "0\n" +
-            "Enable Prefetch streams initiated from STB accesses. This is the reset value.\n" +
-            "\n" +
-            "1\n" +
-            "Disable Prefetch streams initiated from STB accesses.\n" +
-            "\n" +
-            "[21]\tIFUTHDIS\t\n" +
-            "IFU fetch throttle disabled. The possible values are:\n" +
-            "\n" +
-            "0\n" +
-            "Fetch throttle enabled. This is the reset value.\n" +
-            "\n" +
-            "1\n" +
-            "Fetch throttle disabled. This setting increases power consumption.\n" +
-            "\n" +
-            "[20:19]\tNPFSTRM\t\n" +
-            "Number of independent data prefetch streams. The possible values are:\n" +
-            "\n" +
-            "0b00\n" +
-            "1 stream.\n" +
-            "\n" +
-            "0b01\n" +
-            "2 streams. This is the reset value.\n" +
-            "\n" +
-            "0b10\n" +
-            "3 streams.\n" +
-            "\n" +
-            "0b11\n" +
-            "4 streams.\n" +
-            "\n" +
-            "[18]\tDSTDIS\t\n" +
-            "Enable device split throttle. The possible values are:\n" +
-            "\n" +
-            "0\n" +
-            "Device split throttle disabled.\n" +
-            "\n" +
-            "1\n" +
-            "Device split throttle enabled. This is the reset value.\n" +
-            "\n" +
-            "[17]\tSTRIDE\t\n" +
-            "Enable stride detection. The possible values are:\n" +
-            "\n" +
-            "0\n" +
-            "2 consecutive strides to trigger prefetch. This is the reset value.\n" +
-            "\n" +
-            "1\n" +
-            "3 consecutive strides to trigger prefetch.\n" +
-            "\n" +
-            "[16]\t-\tReserved, res0.\n" +
-            "[15:13]\tL1PCTL\t\n" +
-            "L1 Data prefetch control. The value of the this field determines the maximum number of outstanding data prefetches allowed in the L1 memory system, excluding those generated by software load or PLD instructions. The possible values are:\n" +
-            "\n" +
-            "0b000\n" +
-            "Prefetch disabled.\n" +
-            "\n" +
-            "0b001\n" +
-            "1 outstanding prefetch allowed.\n" +
-            "\n" +
-            "0b010\n" +
-            "2 outstanding prefetches allowed.\n" +
-            "\n" +
-            "0b011\n" +
-            "3 outstanding prefetches allowed.\n" +
-            "\n" +
-            "0b100\n" +
-            "4 outstanding prefetches allowed.\n" +
-            "\n" +
-            "0b101\n" +
-            "5 outstanding prefetches allowed. This is the reset value.\n" +
-            "\n" +
-            "0b110\n" +
-            "6 outstanding prefetches allowed.\n" +
-            "\n" +
-            "0b111\n" +
-            "8 outstanding prefetches allowed.\n" +
-            "\n" +
-            "[12:11]\t-\tReserved, res0.\n" +
-            "[10]\tDODMBS\t\n" +
-            "Disable optimized Data Memory Barrier behavior. The possible values are:\n" +
-            "\n" +
-            "0\n" +
-            "Enable optimized Data Memory Barrier behavior. This is the reset value.\n" +
-            "\n" +
-            "1\n" +
-            "Disable optimized Data Memory Barrier behavior.\n" +
-            "\n" +
-            "[9:7]\t-\tReserved, res0.\n" +
-            "[6]\tL1DEIEN\t\n" +
-            "L1 D-cache data RAM error injection enable. The possible values are;\n" +
-            "\n" +
-            "0\n" +
-            "Normal behavior, errors are not injected. This is the reset value.\n" +
-            "\n" +
-            "1\n" +
-            "Double-bit errors are injected on all writes to the L1 D-cache data RAMs for the first word of each 32-byte region.\n" +
-            "\n" +
-            "[5:0]\t-\tReserved, res0.\n";
+    private String regDesc = "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"45\" />" +
+            "        <END_BIT VAL=\"63\"  />" +
+            "        <NAME VAL=\"-\" />" +
+            "        <FUNCTION VAL=\"Reserved\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"44\" />" +
+            "        <END_BIT VAL=\"44\"  />" +
+            "        <NAME VAL=\"ENDCCASCI\" />" +
+            "        <FUNCTION VAL=\"Enable data cache clean as data cache clean/invalidate\" />" +
+            "        <VALID_VALUE VAL=\"1\" VAL_MEANING=\"Executes data cache clean operations as data cache clean and invalidate.\" />" +
+            "        <VALID_VALUE VAL=\"0\" VAL_MEANING=\"Normal behavior, data cache clean operations are unaffected. This is the reset value.\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"31\" />" +
+            "        <END_BIT VAL=\"43\"  />" +
+            "        <NAME VAL=\"-\" />" +
+            "        <FUNCTION VAL=\"Reserved\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"30\" />" +
+            "        <END_BIT VAL=\"30\"  />" +
+            "        <NAME VAL=\"FPDIDIS\" />" +
+            "        <FUNCTION VAL=\"Disable floating-point dual issue\" />" +
+            "        <VALID_VALUE VAL=\"1\" VAL_MEANING=\"Disable dual issue of floating-point, Advanced SIMD and Cryptography instructions.\" />" +
+            "        <VALID_VALUE VAL=\"0\" VAL_MEANING=\"Enable dual issue of floating-point, Advanced SIMD and Cryptography instructions. This is the reset value.\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"29\" />" +
+            "        <END_BIT VAL=\"29\"  />" +
+            "        <NAME VAL=\"DIDIS\" />" +
+            "        <FUNCTION VAL=\"Disable Dual Issue\" />" +
+            "        <VALID_VALUE VAL=\"1\" VAL_MEANING=\"Disable Dual Issue of all instructions.\" />" +
+            "        <VALID_VALUE VAL=\"0\" VAL_MEANING=\"Enable Dual Issue of instructions. This is the reset value.\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"27\" />" +
+            "        <END_BIT VAL=\"28\"  />" +
+            "        <NAME VAL=\"RADIS\" />" +
+            "        <FUNCTION VAL=\"Write streaming no-allocate threshold\" />" +
+            "        <VALID_VALUE VAL=\"0\" VAL_MEANING=\"16th consecutive streaming cache line does not allocate in the L1 or L2 cache.\" />" +
+            "        <VALID_VALUE VAL=\"1\" VAL_MEANING=\"128th consecutive streaming cache line does not allocate in the L1 or L2 cache. This is the reset value.\" />" +
+            "        <VALID_VALUE VAL=\"2\" VAL_MEANING=\"512th consecutive streaming cache line does not allocate in the L1 or L2 cache.\" />" +
+            "        <VALID_VALUE VAL=\"3\" VAL_MEANING=\"Disables streaming. All write-allocate lines allocate in the L1 or L2 cache.\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"25\" />" +
+            "        <END_BIT VAL=\"26\"  />" +
+            "        <NAME VAL=\"L1RADIS\" />" +
+            "        <FUNCTION VAL=\"Write streaming no-L1-allocate threshold\" />" +
+            "        <VALID_VALUE VAL=\"0\" VAL_MEANING=\"4th consecutive streaming cache line does not allocate in the L1 cache. This is the reset value.\" />" +
+            "        <VALID_VALUE VAL=\"1\" VAL_MEANING=\"64th consecutive streaming cache line does not allocate in the L1 cache.\" />" +
+            "        <VALID_VALUE VAL=\"2\" VAL_MEANING=\"128th consecutive streaming cache line does not allocate in the L1 cache.\" />" +
+            "        <VALID_VALUE VAL=\"3\" VAL_MEANING=\"Disables streaming. All write-allocate lines allocate in the L1 cache.\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"24\" />" +
+            "        <END_BIT VAL=\"24\"  />" +
+            "        <NAME VAL=\"DTAH\" />" +
+            "        <FUNCTION VAL=\"Disable Transient Allocation Hint\" />" +
+            "        <VALID_VALUE VAL=\"1\" VAL_MEANING=\"The Transient allocation hint in the MAIR is ignored and treated the same as non-transient allocation types. The LDNP and STNP instructions in AArch64 behave the same as the equivalent LDP and STP instructions. This is the reset value.\" />" +
+            "        <VALID_VALUE VAL=\"0\" VAL_MEANING=\"Normal operation.\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"23\" />" +
+            "        <END_BIT VAL=\"23\"  />" +
+            "        <NAME VAL=\"STBPFRS\" />" +
+            "        <FUNCTION VAL=\"Disable ReadUnique request for prefetch streams initiated by STB accesses\" />" +
+            "        <VALID_VALUE VAL=\"1\" VAL_MEANING=\"ReadShared used for prefetch streams initiated from STB accesses.\" />" +
+            "        <VALID_VALUE VAL=\"0\" VAL_MEANING=\"ReadUnique used for prefetch streams initiated from STB accesses. This is the reset value.\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"22\" />" +
+            "        <END_BIT VAL=\"22\"  />" +
+            "        <NAME VAL=\"STBPFDIS\" />" +
+            "        <FUNCTION VAL=\"Disable prefetch streams initiated from STB accesses\" />" +
+            "        <VALID_VALUE VAL=\"1\" VAL_MEANING=\"Disable Prefetch streams initiated from STB accesses.\" />" +
+            "        <VALID_VALUE VAL=\"0\" VAL_MEANING=\"Enable Prefetch streams initiated from STB accesses. This is the reset value.\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"21\" />" +
+            "        <END_BIT VAL=\"21\"  />" +
+            "        <NAME VAL=\"IFUTHDIS\" />" +
+            "        <FUNCTION VAL=\"IFU fetch throttle disabled.\" />" +
+            "        <VALID_VALUE VAL=\"1\" VAL_MEANING=\"Fetch throttle disabled. This setting increases power consumption.\" />" +
+            "        <VALID_VALUE VAL=\"0\" VAL_MEANING=\"Fetch throttle enabled. This is the reset value.\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"19\" />" +
+            "        <END_BIT VAL=\"20\"  />" +
+            "        <NAME VAL=\"NPFSTRM\" />" +
+            "        <FUNCTION VAL=\"Number of independent data prefetch streams\" />" +
+            "        <VALID_VALUE VAL=\"0\" VAL_MEANING=\"1 stream.\" />" +
+            "        <VALID_VALUE VAL=\"1\" VAL_MEANING=\"2 streams. This is the reset value.\" />" +
+            "        <VALID_VALUE VAL=\"2\" VAL_MEANING=\"3 streams.\" />" +
+            "        <VALID_VALUE VAL=\"3\" VAL_MEANING=\"4 streams.\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"18\" />" +
+            "        <END_BIT VAL=\"18\"  />" +
+            "        <NAME VAL=\"DSTDIS\" />" +
+            "        <FUNCTION VAL=\"Enable device split throttle.\" />" +
+            "        <VALID_VALUE VAL=\"1\" VAL_MEANING=\"Device split throttle enabled. This is the reset value.\" />" +
+            "        <VALID_VALUE VAL=\"0\" VAL_MEANING=\"Device split throttle disabled.\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"17\" />" +
+            "        <END_BIT VAL=\"17\"  />" +
+            "        <NAME VAL=\"STRIDE\" />" +
+            "        <FUNCTION VAL=\"Enable stride detection.\" />" +
+            "\t\t<VALID_VALUE VAL=\"1\" VAL_MEANING=\"3 consecutive strides to trigger prefetch.\" />" +
+            "        <VALID_VALUE VAL=\"0\" VAL_MEANING=\"2 consecutive strides to trigger prefetch. This is the reset value.\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"16\" />" +
+            "        <END_BIT VAL=\"16\"  />" +
+            "        <NAME VAL=\"-\" />" +
+            "        <FUNCTION VAL=\"Reserved\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"13\" />" +
+            "        <END_BIT VAL=\"15\"  />" +
+            "        <NAME VAL=\"L1PCTL\" />" +
+            "        <FUNCTION VAL=\"L1 Data prefetch control. The value of the this field determines the maximum number of outstanding data prefetches allowed in the L1 memory system, excluding those generated by software load or PLD instructions.\" />" +
+            "\t\t<VALID_VALUE VAL=\"0\" VAL_MEANING=\"Prefetch disabled.\" />" +
+            "        <VALID_VALUE VAL=\"1\" VAL_MEANING=\"1 outstanding prefetch allowed.\" />" +
+            "        <VALID_VALUE VAL=\"2\" VAL_MEANING=\"2 outstanding prefetches allowed.\" />" +
+            "        <VALID_VALUE VAL=\"3\" VAL_MEANING=\"3 outstanding prefetches allowed.\" />" +
+            "        <VALID_VALUE VAL=\"4\" VAL_MEANING=\"4 outstanding prefetches allowed.\" />" +
+            "        <VALID_VALUE VAL=\"5\" VAL_MEANING=\"5 outstanding prefetches allowed. This is the reset value.\" />" +
+            "        <VALID_VALUE VAL=\"6\" VAL_MEANING=\"6 outstanding prefetches allowed.\" />" +
+            "        <VALID_VALUE VAL=\"7\" VAL_MEANING=\"8 outstanding prefetches allowed.\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"11\" />" +
+            "        <END_BIT VAL=\"12\"  />" +
+            "        <NAME VAL=\"-\" />" +
+            "        <FUNCTION VAL=\"Reserved\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"10\" />" +
+            "        <END_BIT VAL=\"10\"  />" +
+            "        <NAME VAL=\"DODMBS\" />" +
+            "        <FUNCTION VAL=\"Disable optimized Data Memory Barrier behavior\" />" +
+            "\t\t<VALID_VALUE VAL=\"1\" VAL_MEANING=\"Disable optimized Data Memory Barrier behavior.\" />" +
+            "        <VALID_VALUE VAL=\"0\" VAL_MEANING=\"Enable optimized Data Memory Barrier behavior. This is the reset value.\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"7\" />" +
+            "        <END_BIT VAL=\"9\"  />" +
+            "        <NAME VAL=\"-\" />" +
+            "        <FUNCTION VAL=\"Reserved\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"6\" />" +
+            "        <END_BIT VAL=\"6\"  />" +
+            "        <NAME VAL=\"L1DEIEN\" />" +
+            "        <FUNCTION VAL=\"L1 D-cache data RAM error injection enable\" />" +
+            "\t\t<VALID_VALUE VAL=\"1\" VAL_MEANING=\"Double-bit errors are injected on all writes to the L1 D-cache data RAMs for the first word of each 32-byte region.\" />" +
+            "        <VALID_VALUE VAL=\"0\" VAL_MEANING=\"Normal behavior, errors are not injected. This is the reset value.\" />" +
+            "</BIT_FIELD>" +
+            "<BIT_FIELD>" +
+            "        <START_BIT VAL=\"0\" />" +
+            "        <END_BIT VAL=\"5\"  />" +
+            "        <NAME VAL=\"-\" />" +
+            "        <FUNCTION VAL=\"Reserved\" />" +
+            "</BIT_FIELD>";
 
     public CPUACTLR_DebugRegisters() {
         setRegister_description(regDesc);
